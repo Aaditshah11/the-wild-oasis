dd4b2b4af5896d91b65a5e3f35eb9311182cf243
Aaditshah11 <aaditshah2006@gmail.com>
2026-02-13 23:30:35 +0530

    explore Next

---
+++ app/cabins/page.js
@@ -0,0 +1,17 @@
+async function page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return (
+    <div>
+      <h1>select cabin</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default page;
